e4da8b00f3f05818b9cb38558661ad0cc8e91aec
Szymon Tracz <szymontracz1@gmail.com>
2022-04-19 14:01:29 +0200

    display user questions

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import answers as answers
 from flask import Flask, request, render_template, redirect, url_for
 import data_handler
 
@@ -17,5 +18,23 @@ def list():
     return render_template('list.html', questions=questions, table_headers=table_headers)
 
 
+@app.route('/question/<int:question_id>', methods=['GET', 'POST'])
+def questions(question_id):
+    questions = data_handler.get_data_file('sample_data/question.csv')
+    all_answers = data_handler.get_data_file('sample_data/answer.csv')
+
+    for question in questions:
+        if question['id'] == str(question_id):
+            the_question = question
+    answers = []
+    for answer in all_answers:
+        if answer['question_id'] == str(question_id):
+            answers.append(answer)
+
+    return render_template('questions.html', question=the_question, answers=answers)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        debug=True
+    )

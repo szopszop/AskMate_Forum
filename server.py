ca99929724946a0964b5a7792921ca9d652902b7
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-04-19 14:31:02 +0200

    add question

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import data_handler
 
 
 app = Flask(__name__)
+def add_question(id_ ,questions ):
 
 
 @app.route("/")
@@ -15,6 +16,18 @@ def list():
     questions = data_handler.get_data_file('sample_data/question.csv')
     table_headers = data_handler.build_headers()
     return render_template('list.html', questions=questions, table_headers=table_headers)
+
+@app.route('/add-question', methods=['GET', 'POST'])
+def ask_a_question():
+    if request.method == 'POST':
+        questions = data_handler.get_data_file('sample_data/question.csv')
+
+        for question in questions:
+            question['title'] = request.form['title']
+            question['message'] = request.form['message']
+            table_headers = data_handler.build_headers()
+            return redirect('/')
+    return render_template('add-question')
 
 
 if __name__ == "__main__":

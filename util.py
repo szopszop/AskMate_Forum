4e36fb8fee7d4d383c2f383dfe780216184c021b
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-04-19 17:00:35 +0200

    Refactor code

--- util.py
+++ util.py
@@ -4,10 +4,10 @@ def sort_by(key, order, items):
         case 'title':
             return sorted(items, key=lambda x: x['title'], reverse=order)
         case 'time':
-            return sorted(items, key=lambda x: x['submission_time'], reverse=order)
+            return sorted(items, key=lambda x: int(x['submission_time']), reverse=order)
         case 'message':
             return sorted(items, key=lambda x: x['message'], reverse=order)
         case 'views':
-            return sorted(items, key=lambda x: x['view_number'], reverse=order)
+            return sorted(items, key=lambda x: int(x['view_number']), reverse=order)
         case 'votes':
-            return sorted(items, key=lambda x: x['vote_number'], reverse=order)
+            return sorted(items, key=lambda x: int(x['vote_number']), reverse=order)

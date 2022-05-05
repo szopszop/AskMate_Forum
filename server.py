7112e17702715dc2a4a6ea2186ce61be6e4d4c42
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-05-05 13:59:08 +0200

    Refactor code

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     question = data_manager.display_latest_question()
     return render_template("index.html", questions=question)
 
